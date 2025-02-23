f54f73f8400edb4f6df6a2b65fca1f611ac8ea0f
Tom Dent <tomdent991@hotmail.com>
2025-02-23 20:13:31 +0100

    Last commit tidy

--- src/script.js
+++ src/script.js
@@ -90,10 +90,7 @@ function init() {
   console.log("windmillObjs", windmillObjs);
 
   dragControls = new DragControls([...windmillObjs, windmillTest.object, sphere], camera, canvas)
-  dragControls.transformGroup = true
-  console.log("drag objects", dragControls.objects);
-  
-  // dragControls.enabled = false
+  dragControls.transformGroup = true  
 
   transformControls = new TransformControls(camera, canvas)
   // transformControls.enabled = false
@@ -102,8 +99,6 @@ function init() {
   transformControls.setSize(1)
   transformControls.showX = false
   transformControls.showZ = false
-  // transformControls.setSpace("local")
-
   scene.add(transformControls.getHelper())
 
   raycaster = new THREE.Raycaster()
@@ -128,10 +123,6 @@ transformControls.addEventListener("dragging-changed", onRotate)
 function onRotate(event) {
     dragControls.enabled = !event.value
     orbitControls.enabled = !event.value
-    console.log("tcontrols event", event, transformControls.rotationAngle);
-
-    console.log("transform objeect", transformControls.object.rotation)
-    
 }
 
 function onMouseMove(event) {
@@ -140,25 +131,6 @@ function onMouseMove(event) {
   const objects = windfarm.windmills.map((windmill) => windmill.object)
   
   const intersection = raycaster.intersectObjects(objects, true)
-  // if(intersection.length > 0) {
-  //   if(intersected != intersection[0].object) {
-  //         if ( intersected ) {
-  //           intersected.parent.children.forEach((child) => child.material.emissive.setHex( intersected.currentHex ))
-  //         }
-  //         intersected = intersection[0].object
-  //         intersected.currentHex = intersected.material.emissive.getHex()
-  //         intersected.parent.children.forEach((child) => {
-  //           child.material.emissive.set( "green" )
-  //         })          
-  //       }
-  //     } else {
-  //       if (intersected) {
-  //         intersected.parent.children.forEach((child) => child.material.emissive.setHex(intersected.currentHex))
-  //       };        
-  //       intersected = null;
-  //     }
-  
-
 }
 
 function onClick(event) {
@@ -202,12 +174,6 @@ function onClick(event) {
 const parentEndPos = new Map()
 
 function onDragStart(event) {  
-  // const object = event.object
-  // const worldPos = new THREE.Vector3()
-  // object.getWorldPosition(worldPos)
-  
-  // startingPos.set(object.uuid, worldPos.clone())
-
 }
 
 function onDrag(event) {
@@ -215,134 +181,6 @@ function onDrag(event) {
   console.log("object", object.position);
   
   object.position.y = object.name === "turbineRotor" ? 10 : 0
-
-  // if(object.parent) {
-  //   object.parent.position.x = object.position.x
-  //   object.parent.position.z = object.position.z
-
-  //   console.log("parent position", object.parent.position);
-    
-  //   object.position.set(0, 0, 0);
-  // }
-
-  
-
-//   if (object.parent) {
-    
-//     const lastPosition = parentEndPos.get(object.parent.uuid) || {x: 0, y: 0, z: 0}
-//     console.log("lastPOSITION", lastPosition);
-    
-
-//     object.parent.position.x = lastPosition.x + object.position.x
-//     object.parent.position.z = object.name === "turbineRotor" ? lastPosition.z + object.position.z + 1.005 : lastPosition.z + object.position.z 
-
-//     console.log("parent position", object.parent.position);
-    
-
-//     const children = object.parent.children
-
-//     object.position.set(0, 0, 0)
-
-// }
-
-  // const newPosition = object.position.clone(); // Save new position
-
-  // if (object.parent) {
-  //   object.parent.position.add(newPosition);
-    
-  //   // Reset object's position so it's local to the new parent position
-  //   object.position.set(0, 0, 0);
-  // }
-
-    // children.filter((child) => child.uuid !== object.uuid ).map((child) => {
-    //   child.position.x = object.position.x
-    //   child.position.z = child.name === "turbineRotor" ? object.position.z + 1.005 : object.position.z
-    //   // child.position.set(0, 0, 0)
-    // })
-  // }
-
-  // transformControls._root.updateMatrixWorld()
-
-
-  // console.log("x", object.position.x, "z", object.position.z)
-
-  // const startPos = startingPos.get(object.uuid);
-  // const child = object.parent.children[1];
-  // const childStartPos = startingPos.get(child.uuid);
-
-  // if (!startPos || !childStartPos) return;
-
-  // // Compute object's current world position
-  // const currentWorldPos = new THREE.Vector3();
-  // object.getWorldPosition(currentWorldPos);
-
-  // // Calculate the delta movement in world space
-  // const deltaX = currentWorldPos.x - startPos.x;
-  // const deltaZ = currentWorldPos.z - startPos.z;
-
-  // // Apply the delta to the child's position in world space
-  // const newChildPos = new THREE.Vector3(
-  //   childStartPos.x + deltaX,
-  //   child.position.y, // Keep Y unchanged
-  //   childStartPos.z + deltaZ
-  // );
-
-  // child.position.copy(newChildPos);
-
-  // console.log("Child Updated Pos:", newChildPos);
-
-
-  // const child = object.parent.children[1]
-
-
-
-  // const childWPos = startingPos.get(child.uuid)
-  // // const childWPos = child.getWorldPosition(new THREE.Vector3())
-  // // console.log("objWPos", objWPos, "childWPos", childWPos);
-  
-  // if(!childWPos) return
-
-  // console.log("child world pos", childWPos)
-
-  // const newX = childWPos.x + object.position.x
-  // const newZ = childWPos.z + object.position.z
-  // console.log("newX", newX, "newZ", newZ)
-
-  // child.position.x = newX
-  // child.position.z = newZ
-
-
-  
-  // const startPos = startingPos.get(object.uuid)
-  // if (!startPos) return;
-  
-  // const rotationAngle = event.object.parent.rotation.y  
-
-  // console.log("rotationAngle", rotationAngle);
-  
-
-  // const parent = event.object.parent
-
-  // let localDeltaX = object.position.x
-  // let localDeltaZ = object.position.z
-
-  // const rotatedX = localDeltaX * Math.cos(rotationAngle) - localDeltaZ * Math.sin(rotationAngle);
-  // const rotatedZ = localDeltaX * Math.sin(rotationAngle) + localDeltaZ * Math.cos(rotationAngle);
-  
-  
-  // parent.position.x = startPos.x + rotatedX
-  // parent.position.z = startPos.z + rotatedZ
-
-  // object.position.set(0, object.position.y, 0);
-
-
-  // const deltaX = parent.position.x - startPos.x
-  // const deltaZ = parent.position.z - startPos.z
-
-  // // relative to direction windmill is facing
-  
-  // object.position.x -= deltaX
-  // object.position.z -= deltaZ
   
   orbitControls.enabled = false
   orbitControls.update()
@@ -355,24 +193,7 @@ function onDragEnd(event) {
   const parent = event.object.parent
   const {x, y, z} = parent.position
   parentEndPos.set(parent.uuid, {x, y, z})
-  // startingPos.delete(event.object.uuid);
-
-  // const helper = transformControls.getHelper()
-  // if (!helper) return; // Ensure helper exists
   
-  // helper.position.x = event.object.position.x
-  // helper.position.z = event.object.position.z
-
-  // event.object.geometry.applyMatrix4(new THREE.Matrix4().makeTranslation(event.object.position.x, 0, event.object.position.z))
-  
-  // event.object.geometry.center()
-  console.log("object drag end", event.object.position, event.object.parent.position);
-  
-  // transformControls.reset()
-
-  // transformControls._root.updateMatrixWorld()
-
-
   orbitControls.enabled = true
   orbitControls.update()
 }
